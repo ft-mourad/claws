73c4e62bf5bca16b44ae3d88871fcf82356e7c1f
Mourad <trabelsi.mourad@hotmail.fr>
2017-03-20 12:21:09 +0000

    put iids into array and instances info (including iid) into a different array

--- claws.go
+++ claws.go
@@ -21,47 +21,49 @@ import (
 	"github.com/fatih/color"
 )
 
+type SimpleInstance struct {
+	id    string
+	name  string
+	state string
+	owner string
+}
+
 var (
 	regions = map[string]string{
 		"ireland":   "eu-west-1",
 		"frankfurt": "eu-central-1",
 	}
-
+	instances     []SimpleInstance
 	iids          []string
 	regionTag     = flag.String("r", "eu-central-1", "search tag, region")
 	Tag           = flag.String("t", "Name:*", "search tag, key")
 	keyTag        string
 	valueTag      string
 	instanceId           = flag.String("i", "", "specific instance id")
+	command              = flag.String("c", "", "start or stop command on all instances retrieved by the search")
 	instance_mode string = "false"
 )
 
 // CLI arguments
 
 func parseInput() string {
-
 	//process the arguments
 	var region string
-
 	flag.Parse()
 	if tmp, exist := regions[*regionTag]; exist {
 		region = tmp
 	} else {
 		region = *regionTag
 	}
-
 	parsed_arg := strings.Split(*Tag, ":")
 	keyTag, valueTag = parsed_arg[0], parsed_arg[1]
 	fmt.Println(keyTag, " : ", valueTag)
-
 	if *instanceId != "" && keyTag != "" {
 		fmt.Println(" -i and -k/v are mutualy exclusive. Please use one or the other\n")
 		instance_mode = "true"
 	}
-
-	fmt.Println("instance : ", *instanceId, "\n")
-	fmt.Println("search : ", keyTag, " : ", valueTag, " in ", *regionTag, "\n")
-
+	fmt.Println("run : ", *command)
+	fmt.Println("search : ", *instanceId, "with ", keyTag, " = ", valueTag, " in ", *regionTag, "\n")
 	return region
 }
 
@@ -73,8 +75,38 @@ func EC2_init(region string) *ec2.EC2 {
 	}
 	//initialize session
 	svc := ec2.New(sess, &aws.Config{Region: aws.String(region)})
-
 	return svc
+}
+
+func commandInstances(svc *ec2.EC2) {
+
+	for _, iid := range iids {
+		if *command == "start" {
+			fmt.Println("starting")
+			input := &ec2.StartInstancesInput{
+				InstanceIds: []*string{
+					aws.String(iid),
+				},
+				DryRun: aws.Bool(false),
+			}
+			_, err := svc.StartInstances(input)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if *command == "stop" {
+			fmt.Println("stopping")
+			input := &ec2.StopInstancesInput{
+				InstanceIds: []*string{
+					aws.String(iid),
+				},
+				DryRun: aws.Bool(false),
+			}
+			_, err := svc.StopInstances(input)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
 
 func addTagFilter(keyTag string, valueTag string, param *ec2.DescribeInstancesInput) *ec2.DescribeInstancesInput {
@@ -107,51 +139,48 @@ func addInstanceIDFilter(iid string, param *ec2.DescribeInstancesInput) *ec2.Des
 	return param
 }
 
-// func stopInstances(iids []string){
-//
-//   var param *ec2.StopInstancesInput
-//   param = param.SetInstanceIDs(iids []string)
-// }
+func displaySimpleInstances(SI SimpleInstance) {
+	fmt.Printf("%-40s\t %-60s\t %-60s\t %s\n", SI.id, SI.name, SI.owner, SI.state)
+}
 
-func formatResult(resp *ec2.DescribeInstancesOutput) {
-	var tags = make(map[string]string)
-	for idx, _ := range resp.Reservations {
-		for _, inst := range resp.Reservations[idx].Instances {
-			//var pos int = 0
-			//Here, i couldn't figure out how to find the Name tag on the instance
-			//I just assume if it is not at the 1st position it is at the second
-			//very ugly
-			//if *inst.Tags[0].Key == "Name" {
-			// } else {
-			// 	pos = 1
-			// }
-
-			//to make things nicer i use fatih/color" to colorize the output
-			//depending on the status
-			if *inst.State.Name == "running" {
-				color.Set(color.FgGreen)
-			} else if *inst.State.Name == "stopped" {
-				color.Set(color.FgRed)
-			}
-
-			for i := 0; i <= len(inst.Tags)-1; i++ {
-				tags[*inst.Tags[i].Key] = *inst.Tags[i].Value
-			}
-			fmt.Println(tags["Name"], "\t\t")
-			//fmt.Println(*inst.Tags[0].Key, len(inst.Tags))
-			//iids = append(iids, *inst.InstanceId)
-			//fmt.Printf("%-40s\t %-60s\t %s\n", *inst.InstanceId, *inst.Tags[pos].Value, *inst.State.Name)
-
+func displayResults() {
+	for _, instance := range instances {
+		if instance.state == "running" {
+			color.Set(color.FgGreen)
+		} else if instance.state == "stopped" {
+			color.Set(color.FgRed)
 		}
+		displaySimpleInstances(instance)
 	}
-	//fmt.Println(iids)
-	defer color.Unset() // Use it in your function
 
 }
 
-func searchInstances(svc) {
-	var inputFilter *ec2.DescribeInstancesInput
+func formatResult(resp *ec2.DescribeInstancesOutput) {
+	//var tags = make(map[string]string)
+	var instance SimpleInstance
+	for idx, _ := range resp.Reservations {
+		for _, inst := range resp.Reservations[idx].Instances {
+			instance.state = *inst.State.Name
+			instance.id = *inst.InstanceId
+			for i := 0; i < len(inst.Tags); i++ {
+				if *inst.Tags[i].Key == "Name" {
+					instance.name = *inst.Tags[i].Value
+				}
+				if *inst.Tags[i].Key == "Owner" {
+					instance.owner = *inst.Tags[i].Value
+				}
+			}
+			iids = append(iids, instance.id)
+			instances = append(instances, instance)
+		}
+	}
+	//fmt.Println(iids)
+	displayResults()
+	defer color.Unset() // Use it in your function
+}
 
+func searchInstances(svc *ec2.EC2) {
+	var inputFilter *ec2.DescribeInstancesInput
 	//define inputs (filters)
 	//The type DescribeInstancesInput obviously contains the inputs (here filter)
 	//for the DescribeInstances below
@@ -161,23 +190,20 @@ func searchInstances(svc) {
 	case "true":
 		inputFilter = addInstanceIDFilter(*instanceId, inputFilter)
 	}
+	//call
 	resp, err := svc.DescribeInstances(inputFilter)
 	if err != nil {
 		panic(err)
 	}
-
 	formatResult(resp)
 }
 
 //need an index func to put into slices/maps the search result.
 //then we'll be able to run command against the Instances
 func main() {
-
 	region := parseInput()
 	svc := EC2_init(region)
 
 	searchInstances(svc)
-	//run the 'list Instances command'
-	//resp is of type DescribeInstancesOutput
-
+	commandInstances(svc)
 }
